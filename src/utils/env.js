896968b518983abb203806f19eb41035eabeaa3b
Long Pham <phamlong0711@gmail.com>
2019-11-27 18:52:36 +0700

    Update API server

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,6 @@
 export default {
   STORAGE_KEY: 'bc-storage',
-  API_SERVER: 'http://localhost:712/candidates',
+  API_SERVER: process.env.NODE_ENV === 'production' ? 'https://vws-server.herokuapp.com/candidates' : 'http://localhost:712/candidates',
   API: {
     GET_CANDIDATES: '/getAll',
     CREATE_CANDIDATE: '/create'
